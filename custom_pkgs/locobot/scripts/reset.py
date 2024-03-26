946e9c17b45dedf5bc04cfbee3886f10ea7d784a
SergeyuiL <shugaoliu@outlook.com>
2024-03-26 15:13:58 +0800

    modified locobot.launch

--- custom_pkgs/locobot/scripts/reset.py
+++ custom_pkgs/locobot/scripts/reset.py
@@ -1,13 +1,17 @@
+#!/usr/bin/env python3
+
 from interbotix_xs_modules.locobot import InterbotixLocobotXS
 
 
 def main():
     locobot = InterbotixLocobotXS(robot_model="locobot_wx250s", arm_model="mobile_wx250s", use_move_base_action=True)
     
+    locobot.arm.go_to_home_pose()
+    locobot.gripper.open()
     locobot.gripper.close()
     locobot.arm.go_to_sleep_pose()
 
-    # locobot.camera.pan_tilt_go_home()
+    locobot.camera.pan_tilt_go_home()
     locobot.camera.pan_tilt_move(0, 0.2618)
 
 if __name__=='__main__':
